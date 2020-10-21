f9bd09f6c8e8bbd33dae62b55f54c503da71ff35
Cameron Parker <Cameronleeparker01@gmail.com>
2020-10-21 10:12:17 -0700

    new

---
+++ 1app.js
@@ -0,0 +1,56 @@
+var myDog = {
+    "name": "Whiskers",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Water", "Dogs"]
+  };
+
+  console.log(myDog.tails);
+
+    var myTurtle = {
+    "name": "Cam",
+    "legs": 9,
+    "tails": 5,
+    "friends" : ["Bugs", "Water"]
+    };
+    console.log(myTurtle.name)
+    
+var myBird = {
+    "name": "todd",
+    "legs": 3,
+    "tails": 2,
+    "friends" : ["Rain", "Dogs"]
+  }; 
+      console.log(myBird.legs)
+
+
+
+  let myDog = [12,23, 34]
+
+    function logArray(arr){
+
+    for(var i = 0; i < arr.length; i++){
+      console.log(arr[i]);
+    }
+  }
+        
+    logArray(myDog);
+
+    let myBird = [13,15, 45]
+
+      function logArray(arr){
+        
+        for(var i = 0; i < arr.length; i++){
+          console.log(arr[i]);
+        }
+      }
+        logArray(myBird);
+
+        let myTurtle = [1, 12, 94]
+
+        function logArray(arr){
+          for(var i = 0; i < arr.length; i++){
+            console.log(arr[i]);
+          }
+        }
+          logArray(myTurtle);
